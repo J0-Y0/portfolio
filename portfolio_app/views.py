69dee6e1e79e1373c48abf7b56961befca0a7970
yosef <yosfemyau@gmail.com>
2024-11-02 00:15:46 +0300

    url routing

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, "index.html")
+def dynamic_page(request, page_name):
+    # Mapping page names to template files
+    pages = {
+        "home": "home.html",
+        "education": "education.html",
+        "experience": "experience.html",
+        "about": "about.html",
+    }
+
+    # Get the template for the requested page or default to 'home.html'
+    template_to_include = pages.get(page_name, "home.html")
+
+    return render(request, "index.html", {"page": template_to_include})
