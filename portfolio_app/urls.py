eed79b673948e0bf5d368fcac3b0ca8083ceb6e7
yosef <yosfemyayu@gmail.com>
2024-11-02 13:02:21 +0300

    add a landing page

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.dynamic_page, name="dynamic_page"),
     path("<str:page_name>", views.dynamic_page, name="dynamic_page"),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 
 
-def dynamic_page(request, page_name):
+def dynamic_page(request, page_name=""):
     # Mapping page names to template files
     pages = {
-        "home": "home.html",
+        "": "landing.html",
         "education": "education.html",
         "experience": "experience.html",
         "about": "about.html",
+        "projects": "projects.html",
     }
 
     # Get the template for the requested page or default to 'home.html'
-    template_to_include = pages.get(page_name, "home.html")
+    template_to_include = pages.get(page_name)
 
     return render(request, "index.html", {"page": template_to_include})
