91f295da96b80ece306084776df9b84e664c6c6f
yosef <yosfemyayu@gmail.com>
2024-11-07 17:51:55 +0300

    Update Education and Certification pages: Simplify layout and enhance content.

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -8,7 +8,7 @@ def dynamic_page(request, page_name=""):
         "education": "education.html",
         "experience": "experience.html",
         "about": "about.html",
-        "projects": "projects.html",
+        "projects": "projects_detail.html",
         "contact": "contact.html",
     }
 
