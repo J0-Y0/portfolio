5e909c265d2d6d6d5a300749d11ecfffec83547a
yosef <yosfemyau@gmail.com>
2025-01-16 12:20:36 +0300

    populating education and certification data from db

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -66,9 +66,9 @@ class Education(models.Model):
     degree = models.CharField(max_length=100)
     start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
-    website = models.URLField(max_length=300)
+    website = models.URLField(max_length=300, blank=True)
     address = models.CharField(max_length=255)
-    details = models.TextField()
+    details = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.degree} from {self.institution}"
