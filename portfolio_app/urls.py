a9db3381c57def71a64f1bc0cc43f37628985b9a
yosef <yosfemyau@gmail.com>
2025-04-30 13:13:27 +0300

    fix project page layout issue

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -9,14 +9,15 @@ from . import views
 
 urlpatterns = [
     path("", views.landing, name="landing"),
-    path("about/", views.about, name="about"),
-    path("education/", views.education, name="education"),
-    path("experience/", views.experience, name="experience"),
-    path("projects/", views.projects, name="projects"),
-    path("contact/", views.contact, name="contact"),
-    # path("message/", views.message, name="contact"),
-    path("flowbit/", views.flowbit, name="flowbit"),
+    # path("about/", views.about, name="about"),
+    # path("education/", views.education, name="education"),
+    # path("experience/", views.experience, name="experience"),
+    # path("projects/", views.projects, name="projects"),
+    # path("contact/", views.contact, name="contact"),
+    # # path("message/", views.message, name="contact"),
+    # path("flowbit/", views.flowbit, name="flowbit"),
     path("project_detail/<str:slug>/", views.project_detail, name="project_detail"),
+    path("index/", views.index, name="index"),
 ]
 
 if settings.DEBUG:
--- portfolio_app/views copy.py
+++ portfolio_app/views copy.py
@@ -25,15 +25,61 @@ def landing(request):
     return render(request, "pages/landing.html", context)
 
 
-def index(request):
+def about(request):
     context = get_common_context()
-    return render(request, "index.html", context)
+    return render(request, "pages/about.html", context)
+
+
+def education(request):
+    context = get_common_context()
+    return render(request, "pages/education.html", context)
+
+
+def experience(request):
+    context = get_common_context()
+    return render(request, "pages/experience.html", context)
+
+
+def projects(request):
+    context = get_common_context()
+    return render(request, "pages/portfolio.html", context)
 
 
 def project_detail(request, slug):
     context = get_common_context()
     # context.update({"project_slug": slug})  # Add slug-specific context if needed
     return render(request, "pages/project_detail.html", context)
+
+
+# def message(request):
+#     if request.method == "POST":
+#         # Extract data from the POST request
+#         message = request.POST.get("message")
+#         email = request.POST.get("email")
+
+#         # Validate that email and message are not empty
+#         if not email or not message:
+#             return JsonResponse(
+#                 {"error": "Email and message cannot be empty."}, status=400
+#             )
+
+#         # Validate email format
+#         try:
+#             validate_email(email)  # Check if the email is valid
+#         except ValidationError:
+#             return JsonResponse({"error": "Invalid email format."}, status=400)
+
+#         # Save the message to the database
+#         try:
+#             Inbox.objects.create(sender_email=email, message=message)
+#             return JsonResponse({"message": "Message sent successfully!"}, status=200)
+#         except Exception as e:
+#             print(e)
+#             return JsonResponse(
+#                 {"error": "An error occurred while saving the message."}, status=500
+#             )
+
+#     return JsonResponse({"error": "Invalid request method."}, status=405)
 
 
 def contact(request):
@@ -65,3 +111,16 @@ def contact(request):
                 context["error"] = "An error occurred while saving the message."
 
     return render(request, "pages/contact.html", context)
+
+
+def flowbit(request):
+    return render(
+        request,
+        "flowbit.html",
+    )
+
+
+def project_detail(request, slug):
+    context = get_common_context()
+
+    return render(request, "pages/project_detail.html", context)
